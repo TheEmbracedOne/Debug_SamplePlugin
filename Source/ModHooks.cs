9b9a4a8b4cfdbea4fa46db3f505ea0f2e8a4356a
TheEmbracedOne <theembracedone@hallownest.net>
2017-10-04 21:38:48 +0100

    Initial commit

---
+++ Source/ModHooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DebugMod
+{
+    class ModHooks
+    {
+        public static void ModLog(string message)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("@E:\\DEBUG.txt", true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
